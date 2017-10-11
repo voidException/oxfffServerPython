3356c3b216d4e1d41bf4bba40173a89f108d4959
voidException <noexception@126.com>
2017-10-11 15:04:17 +0800

    news table

--- cmdb/models.py
+++ cmdb/models.py
@@ -1,6 +1,9 @@
+
 from django.db import models
 import json
 # Create your models here.
+from django.db.models.functions import datetime
+
 
 class UserInfor (models.Model):
     user=models.CharField(max_length=32)
@@ -22,9 +25,23 @@ class ScienceNews(models.Model):
     crawl_date = models.CharField(max_length=35, null=True)
     from_url = models.CharField(max_length=350, null=True)
 
+######### 葡萄互助数据库表 ############
 
-#####################
-
+class news(models.Model):
+    id=models.BigAutoField(primary_key=True)
+    newsuuid=models.CharField(max_length=60)
+    author=models.CharField(max_length=30,null=True)
+    publishdate=models.DateTimeField('保存日期', default = datetime.timezone.now)
+    title=models.CharField(max_length=60,null=True)
+    vicetitle=models.CharField(max_length=60,null=True)
+    source=models.CharField(max_length=60,null=True)
+    newscontent=models.CharField(max_length=5000,null=True)
+    imageone=models.CharField(max_length=600,null=True)
+    imagetwo=models.CharField(max_length=600,null=True)
+    imagethree=models.CharField(max_length=600,null=True)
+    newsurl=models.CharField(max_length=600,null=True)
+    newstype=models.CharField(max_length=10,null=True)
+    lable=models.CharField(max_length=60,null=True)
 
 
 
